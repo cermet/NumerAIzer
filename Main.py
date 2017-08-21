f57c92d81b77ae257f71f6b96430107cecd097a2
Cermet <cermetk@gmail.com>
2017-08-21 11:47:39 -0400

    0.2 Complete

--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn import linear_model, svm
+from xgboost import XGBClassifier
 import os
 
 def main():
@@ -12,12 +14,18 @@ def main():
     #import traing data as a pandas data frame, ignore first 3 cols as they are not features
     df = pd.read_csv('.\\Data\\numerai_training_data.csv', header=0, usecols=list(range(3, 25)))
 
-    X = df.iloc[:,0:20].as_matrix()
-    Y = df.iloc[:,21].as_matrix()
+    #split data into features and target
+    X = df.iloc[:,0:21].as_matrix()
+    y = df.iloc[:,21].as_matrix()        
+        
+    lr = linear_model.LogisticRegression()
+    lrScores = cross_val_score(lr, X, y, cv=10, scoring = 'f1')
+    sum(lrScores) / len(lrScores)
     
-    kf = KFold(n_splits=10)
-    kf.get_n_splits(X)
+    xgb = XGBClassifier()
+    xgbScores = cross_val_score(xgb, X, y, cv=10, scoring = 'f1')
+    sum(xgbScores) / len(xgbScores)
     
-    for train_index, test_index in kf.split(X):
-        X_train, X_test = X[train_index], X[test_index]
-        Y_train, Y_test = Y[train_index], Y[test_index]
+    clf = svm.SVR()
+    svmScores = cross_val_score(clf, X, y, cv=10)
+    sum(svmScores) / len(svmScores)
